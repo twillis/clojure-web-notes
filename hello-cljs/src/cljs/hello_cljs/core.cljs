81fb94f9b77ffd6288768c641a092c75e7d1ee15
Tom Willis <twillis@topgunhq.com>
2014-01-01 19:32:14 -0500

    clojurescript sample

--- hello-cljs/src/cljs/hello_cljs/core.cljs
+++ hello-cljs/src/cljs/hello_cljs/core.cljs
@@ -4,7 +4,23 @@
 ;;; if you can add files containing macros and compile-time only
 ;;; functions in the :source-paths setting of the :builds, it is
 ;;; strongly suggested to add them to the leiningen :source-paths.
-(ns hello-cljs.core)
+(ns hello-cljs.core
+  (:use [jayq.core :only [$ css html]])
+  (:require [cljsbinding :as binding]
+            [goog.dom :as dom]))
+
+
+;; todo:
+;; - 2 way binding
+;; bind atom
+;; bind map
+;; bind sequence
+
+
+;; - events
+;; - routing
+;; - client side templates
+
 
 (defn foo [greeting]
   (if greeting 
@@ -13,5 +29,7 @@
 
 (.write js/document (foo "Welcome to "))
 
+;; get element and write to console
+(.log js/console (dom/getElement "test"))
 
-
+(.log js/console ($ "#test"))
